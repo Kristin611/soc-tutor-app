1e3fc99efce709f8a95e9cde450a13c89ff7df37
Kris10 <kmrichie611@gmail.com>
2025-03-04 17:10:40 -0600

    tested db + db migrations

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //Alternative way to do a Post request
+  // @Post('/name')
+  // async addName(@Body() body: string) {
+  //   console.log('body:', body)
+  //   return 'Yay!';
+  // }
+
+  @Post('/user')
+  async addUser(@Body() userData: {name: string; username: string; email: string; password: string}) {
+    console.log('body:', userData)
+    return await this.appService.addUser(userData);
+  }
+
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUsers() {
+    return this.appService.getUsers();
   }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,51 @@
 // import viteLogo from '/vite.svg'
 import axios from 'axios'
 import './App.css'
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button, Input } from '@chakra-ui/react'
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState('kristin');
+  const [username, setUsername] = useState('kristin4');
+  const [email, setEmail] = useState('kristin4@gmail.com');
+  const [password, setPassword] = useState('test');
+
+  const handleNameChange = (event: any) => {
+    // console.log('EVENT:', event)
+    // console.log('EVENT-TARGET:', event.target.value);
+    setName(event.target.value);
+  }
+
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value)
+  };
+
+  const handleEmailChange = (event: any) => {
+    setEmail(event.target.value)
+  };
+
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
   
   const handleClick = async () => {
-    const response = await axios.get('http://localhost:3000')
+    const response = await axios.post('http://localhost:3000/user', {
+      name, //the body to be sent via the request
+      username,
+      email,
+      password
+    })
     console.log('RESPONSE:', response.data)
   }
 
   return (
     <>
-      <Box>
-        <Button onClick={handleClick}>Click Me</Button>
+      <Box m={10} display='flex' flexDirection='column'  gap={4}>
+        <Input onChange={handleNameChange} placeholder='Type in a name'/>
+        <Input onChange={handleUsernameChange} placeholder='Type in a username'/>
+        <Input onChange={handleEmailChange} placeholder='Type in an email'/>
+        <Input onChange={handlePasswordChange} placeholder='Type in a password'/>
+        <Button onClick={handleClick}>Add User</Button>
       </Box>
     </>
   )
