a82ed8062e292f7f69751b29c3577b75283dcb3e
Kris10 <kmrichie611@gmail.com>
2025-03-24 15:57:40 -0500

    re-committing changes to main, auth controller and service, and package.json

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
     signUp(signUpDto) {
-        console.log('SIGN UP DTO:', signUpDto)
+        // console.log('SIGN UP DTO:', signUpDto) the data will show up in the nestjs console
+        return signUpDto; //data will show up in the inspect browser console under data
     }
 }
